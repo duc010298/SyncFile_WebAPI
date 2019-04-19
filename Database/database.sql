54ca6a5702af4f0027938a752d9c4b26e8334454
Đỗ Trung Đức <duc010298@gmail.com>
2019-04-19 10:50:28 +0700

    Update database.sql

--- Database/database.sql
+++ Database/database.sql
@@ -1,3 +1,4 @@
+SET timezone TO 'UTC';
 create extension if not exists "uuid-ossp";
 -- user manager
 create table if not exists app_user
@@ -5,7 +6,7 @@ create table if not exists app_user
   user_id               uuid default uuid_generate_v4() primary key,
   user_name             varchar(36)  not null unique,
   encrypted_password    varchar(128) not null,
-  token_active_after   date not null default current_date
+  token_active_after    timestamp not null default now()
 );
 create table if not exists app_role
 (
@@ -20,7 +21,6 @@ create table if not exists user_role
   constraint user_role_fk1 foreign key (user_id) references app_user (user_id),
   constraint user_role_fk2 foreign key (role_id) references app_role (role_id)
 );
-
 --
 insert into app_user (user_id, user_name, encrypted_password, token_active_after)
 values ('44260380-cc98-4296-acac-1d57b88b2535', 'duc010298', '$2a$10$hjBz774Yg4Fff44DYseK4.w4p27w2enR0W.QxSxlIXA.TcxS2bYV.', default);
