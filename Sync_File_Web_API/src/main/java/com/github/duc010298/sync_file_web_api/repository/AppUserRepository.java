9684eafc98b8ea95b2e608a62010720378158a88
Đỗ Trung Đức <duc010298@gmail.com>
2019-04-19 08:56:24 +0700

    rebuild project web api

--- Sync_File_Web_API/src/main/java/com/github/duc010298/sync_file_web_api/repository/AppUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.duc010298.sync_file_web_api.repository;
-
-import com.github.duc010298.sync_file_web_api.entity.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-
-	AppUser findByUserName(String userName);
-}
